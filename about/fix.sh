69d89a1d86b0ee6bfa0a740f8fce414202b5f272
Jun Matsushita <jun@iilab.org>
2016-04-18 23:00:22 +0200

    Clean and added architecture

--- about/fix.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-find -L . -name '*.jpg' -exec ditto \{\} ../_site/openintegrity.org/{\} \;
-find -L . -name '*.png' -exec ditto \{\} ../_site/openintegrity.org/{\} \;
-find -L . -name '*.svg' -exec ditto \{\} ../_site/openintegrity.org/{\} \;
-cp -R ../_tmp/design/mockup ../_site/openintegrity.org/
